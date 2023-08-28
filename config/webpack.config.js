687b2db4470ad07e748c68dc2c61cbf46faca943
Yoriiis <joris.daniel@gmail.com>
2023-08-29 00:10:31 +0200

    Clean webpack Update package

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -72,26 +72,6 @@ module.exports = (env, argv) => {
 			alias: {
 				shared: resolveApp('/src/shared'),
 				package: resolveApp('../vlite')
-				// 'images/layers.png$': path.resolve(
-				// 	__dirname,
-				// 	'../node_modules/leaflet/dist/images/layers.png'
-				// ),
-				// 'images/layers-2x.png$': path.resolve(
-				// 	__dirname,
-				// 	'../node_modules/leaflet/dist/images/layers-2x.png'
-				// ),
-				// 'images/marker-icon.png$': path.resolve(
-				// 	__dirname,
-				// 	'../node_modules/leaflet/dist/images/marker-icon.png'
-				// ),
-				// 'images/marker-icon-2x.png$': path.resolve(
-				// 	__dirname,
-				// 	'../node_modules/leaflet/dist/images/marker-icon-2x.png'
-				// ),
-				// 'images/marker-shadow.png$': path.resolve(
-				// 	__dirname,
-				// 	'../node_modules/leaflet/dist/images/marker-shadow.png'
-				// )
 			}
 		},
 		devServer: {
